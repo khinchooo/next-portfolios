eab020862974bf10b6a41dc7d4d2c330b81c46ac
khinchooo <khinchooo@mm.cybermissions.co.jp>
2021-12-15 17:48:56 +0900

    final commits

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,36 +1,37 @@
 import Link from "next/link"
-import Image from 'next/image'
-import matter from "gray-matter"
+import Image from "next/image"
 import Layout from "../components/layout"
+import matter from "gray-matter"
 import * as style from "../styles/blog.module.scss"
 
-
-const Blog = (props) => {
+const Blog = ({ blogs }) => {
   return (
     <Layout>
       <div className={style.wrapper}>
         <div className={style.container}>
           <h1>Blog</h1>
           <p>エンジニアの日常生活をお届けします</p>
-          {props.blogs.map((blog, index) =>
-            <div key={index} className={style.blogCard}>
-              <div className={style.textContainer}>
-                <h3>{blog.frontmatter.title}</h3>
-                <p>{blog.frontmatter.excerpt}</p>
-                <p>{blog.frontmatter.date}</p>
-                <Link href={`/blog/${blog.slug}`}>
-                  <a>Read More</a>
-                </Link>
+          {blogs.map((blog, index) => {
+            const { title, date, excerpt, image } = blog.frontmatter
+            return (
+              <div key={index} className={style.blogCard}>
+                <div className={style.textContainer}>
+                  <h3>{title}</h3>
+                  <p>{excerpt}</p>
+                  <p>{date}</p>
+                  <Link href={`/blog/${blog.slug}`}><a>Read More</a></Link>
+                </div>
+                <div className={style.cardImg}>
+                  <Image src={image} alt="card-image" height={300} width={1000} quality={90} />
+                </div>
               </div>
-              <div className={style.cardImg}>
-                <Image src={blog.frontmatter.image} alt="card-image"
-                  height={300} width={1000} quality={90} />
-              </div>
-            </div>
+            )
+          }
           )}
         </div>
       </div>
     </Layout>
+
   )
 }
 
@@ -45,7 +46,6 @@ export async function getStaticProps() {
       let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
       const value = values[index]
       const document = matter(value.default)
-      // console.log(document)
       return {
         frontmatter: document.data,
         slug: slug
@@ -53,12 +53,14 @@ export async function getStaticProps() {
     })
     return data
   })(require.context('../data', true, /\.md$/))
-  const orderedBlogs = blogs.sort((a, b) => {
+
+  const orderBlogs = blogs.sort((a, b) => {
     return b.frontmatter.id - a.frontmatter.id
   })
   return {
     props: {
-      blogs: JSON.parse(JSON.stringify(orderedBlogs))
+      blogs: JSON.parse(JSON.stringify(orderBlogs))
     },
+
   }
 }
--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,23 +1,29 @@
-import Image from 'next/image'
-import matter from 'gray-matter'
-import ReactMarkdown from 'react-markdown'
+import matter from "gray-matter"
+import Image from "next/image"
 import Layout from "../../components/layout"
+import ReactMarkdown from 'react-markdown'
 import * as style from "../../styles/singleBlog.module.scss"
 
-const SingleBlog = (props) => {
+const SingleBlog = ({ frontmatter, markdownBody }) => {
+  const { title, date, image } = frontmatter
   return (
     <Layout>
       <div className={style.hero}>
-        <Image src={props.frontmatter.image} alt="blog-image"
-          height={500} width={1000}
-        />
-        <h1>{props.frontmatter.title}</h1>
-        <p>{props.frontmatter.date}</p>
-        <ReactMarkdown children={props.markdownBody} />
+        <Image src={image} alt="blog-image" height="500" width="1000" />
+      </div>
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <h1>{title}</h1>
+          <p>{date}</p>
+          <ReactMarkdown>
+            {markdownBody}
+          </ReactMarkdown>
+        </div>
       </div>
     </Layout>
   )
 }
+
 export default SingleBlog
 
 export async function getStaticPaths() {
@@ -25,20 +31,25 @@ export async function getStaticPaths() {
     const keys = context.keys()
     const data = keys.map((key, index) => {
       let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
+
       return slug
     })
     return data
   })(require.context('../../data', true, /\.md$/))
-  const paths = blogSlugs.map((blogSlugs) => `/blog/${blogSlugs}`)
+
+  const paths = blogSlugs.map((blogSlug) => `/blog/${blogSlug}`)
+
   return {
     paths: paths,
     fallback: false,
   }
 }
+
 export async function getStaticProps(context) {
   const { slug } = context.params
   const data = await import(`../../data/${slug}.md`)
   const singleDocument = matter(data.default)
+
   return {
     props: {
       frontmatter: singleDocument.data,
